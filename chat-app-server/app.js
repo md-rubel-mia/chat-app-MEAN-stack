114f8174654c8a9ee2c59f6b792420f7318528f6
Rubel Mia <mdrobin.eng@gmail.com>
2021-06-07 01:16:06 +0600

    basic structure added

---
+++ chat-app-server/app.js
@@ -0,0 +1,53 @@
+// External imports
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+//internal imports
+const loginRouter = require('./router/loginRouter');
+const usersRouter = require('./router/usersRouter');
+const inboxRouter = require('./router/inboxRouter');
+
+const {notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler");
+
+const app = express();
+dotenv.config();
+
+// db connection
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Database connection successful"))
+.catch(err => console.log(err));
+
+// request parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error Handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`App listening to port: ${process.env.PORT}`);
+})
+
+
