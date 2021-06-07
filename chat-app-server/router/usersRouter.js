ea5afbc66da6215a9bf1b27fb1cfa543b35138b3
Rubel Mia <mdrobin.eng@gmail.com>
2021-06-08 01:58:03 +0600

    user model with file upload

--- chat-app-server/router/usersRouter.js
+++ chat-app-server/router/usersRouter.js
@@ -4,10 +4,13 @@ const express = require("express");
 // internal imports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUserValidators, addUserValidationHandler } = require("../middlewares/users/userValidators");
 const router = express.Router();
 
 // user page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
+router.post("/", avatarUpload, addUserValidators, addUserValidationHandler);
 
 module.exports = router;
