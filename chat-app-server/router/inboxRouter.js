5fec7c4510da3123bb7f287628d34f8131eca7fa
Rubel Mia <mdrobin.eng@gmail.com>
2021-06-10 02:08:38 +0600

    authentication

--- chat-app-server/router/inboxRouter.js
+++ chat-app-server/router/inboxRouter.js
@@ -3,10 +3,12 @@ const express = require("express");
 
 // internal imports
 const { getInbox } = require("../controller/inboxController");
-const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
-const router = express.Router();
-// inbox page
-router.get('/', decorateHtmlResponse("Inbox"), getInbox);
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middlewares/common/checkLogin");
 
+const router = express.Router();
+
+// inbox page
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 module.exports = router;
