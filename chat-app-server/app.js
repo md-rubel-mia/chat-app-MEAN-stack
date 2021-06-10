3a5cd429712a4b52b24457b3bfd6b8e55e100d2b
Rubel Mia <mdrobin.eng@gmail.com>
2021-06-11 03:08:31 +0600

    messaging

--- chat-app-server/app.js
+++ chat-app-server/app.js
@@ -1,30 +1,46 @@
-// External imports
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-//internal imports
-const loginRouter = require('./router/loginRouter');
-const usersRouter = require('./router/usersRouter');
-const inboxRouter = require('./router/inboxRouter');
+// external imports
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
-const {notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler");
+// internal imports
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
 
-// db connection
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("Database connection successful"))
-.catch(err => console.log(err));
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
 
-// request parser
+// set comment as app locals
+app.locals.moment = moment;
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch((err) => console.log(err));
+
+// request parsers
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
@@ -43,11 +59,9 @@ app.use("/inbox", inboxRouter);
 // 404 not found handler
 app.use(notFoundHandler);
 
-// common error Handler
+// common error handler
 app.use(errorHandler);
 
-app.listen(process.env.PORT, ()=> {
-    console.log(`App listening to port: ${process.env.PORT}`);
-})
-
-
+server.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
--- chat-app-server/models/people.js
+++ chat-app-server/models/people.js
@@ -1,37 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const peopleSchema = mongoose.Schema({
+const peopleSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
     },
     mobile: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     avatar: {
-        type: String
+      type: String,
     },
     role: {
-        type: String,
-        enum: ["admin", "user"],
-        default: 'user'
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
     },
-},
-{
-    timeStamps: true
-}
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const People = mongoose.model("People", peopleSchema);
